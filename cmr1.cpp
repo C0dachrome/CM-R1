e083bb501e979bbea7fb0d1bf3fe6fd7a1fb9f8a
Cody Millward <codycmillward@gmail.com>
2026-04-23 10:29:04 -0600

    idk what im doing

--- cmr1.cpp
+++ cmr1.cpp
@@ -3,18 +3,49 @@
  * Comment block for the radar
  * 
  *****************************************************************/
+#include <iostream>
+#include <vector>
+#include <complex>
+#include <cstdint>
+#include <cmath>
 
+double sampleRate = 10e6;
+double duration = 0.001;
+double bandwidth = 50e6;
+double startFreq = 0;
+
+
+//array for lock frequencies
+long long int freqs[3] = { 5.735e9, 5.775e9, 5.815e9 };
+
+long int numSamps = static_cast<int>(sampleRate * duration); //arbitrary number for now
+
+//tx buffer is int16_t, we convert to IQ later
+std::vector<int16_t> txBuffer;
+
+double chirpRate = bandwidth / duration;
+
+// //start with chirp as floats then we convert to int16_t later
+// std::vector<std::complex<float>> chirpFloat(numSamps);
 
 void initRadar(){
     //init IIO
-    //
+    
     //calibrate fast lock by iterating
     //through freqs, locking pll, and 
     //saving to fast lock profile.
-    //
+    for(auto& freq : freqs){
+        //just a cout for debugging
+        std::cout << "locking PLL to " << freq << "hz";
+        //lock PLL to freq
+        //initialize_fastlock_save
+        
+    }
+    
     //generate chirp in array
-    //
+    
     //create tx buffer with cyclic=true
+    // txBuffer.reserve(numSamps * 2);
     //
     //push the buffer
 
@@ -38,4 +69,5 @@ int main(){
     //  find rx phase diff
     //  convert to angle
     //  format packet and push via udp
+    initRadar();
 }
